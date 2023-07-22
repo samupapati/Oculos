d3a81e7fe1a4531dcec50316937acff8c4115324
samupapati <samuelpapati@gmail.com>
2023-07-22 17:35:59 -0300

    add data

---
+++ src/data/products-data.js
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        id: 0,
+        name: 'oculos 1',
+        value: 60,
+    },
+    {
+        id: 1,
+        name: 'oculos 2',
+        value: 20,
+    },
+    {
+        id: 2,
+        name: 'oculos 3',
+        value: 15,
+    },
+]
